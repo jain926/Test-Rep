bd1d563b2f2142af435d3ee240547050163f2b2f
Lakshya Jain <lakshyajain@Lakshyas-MacBook-Pro.local>
2020-05-02 01:10:14 +0530

    Initial Commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Lakshya Jain on 02/05/20.
+//  Copyright © 2020 Lakshya Jain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+//            Image(systemName: "sun.fill")
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
